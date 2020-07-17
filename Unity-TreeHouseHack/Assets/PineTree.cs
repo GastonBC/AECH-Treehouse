5522850c6b5b6ec305524592e96459078c0d4a0d
GastonWkLaptop <gastonbalparda@gmail.com>
2020-07-17 12:32:31 -0300

    Added Brett deck

---
+++ Unity-TreeHouseHack/Assets/PineTree.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineTree : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && this.gameObject.activeInHierarchy)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
